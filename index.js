2a8e05f990b600e912c3b08ec7baa88c48ce02a6
Shaun <shaunjp@shaun-pelling.com>
2018-09-05 21:57:24 +0100

    lesson-18

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const data = [
-  {width: 200, height: 100, fill: 'purple'}
+  {width: 200, height: 100, fill: 'purple'},
+  {width: 100, height: 60, fill: 'pink'},
+  {width: 50, height: 30, fill: 'red'}
 ];
 
-d3.select('rect')
+// console.log(d3.selectAll('rect').data(data))
+
+d3.selectAll('rect') 
   .data(data)
   .attr('width', (d,i,n) => d.width)
   .attr('height', (d,i,n) => d.height)
