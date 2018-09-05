b8416469628803cb8432da8c2dae06f90cd1e31c
Shaun <shaunjp@shaun-pelling.com>
2018-09-05 17:22:48 +0100

    lesson-16

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-const canvas = d3.select('.canvas');
+const data = [
+  {width: 200, height: 100, fill: 'purple'}
+];
 
-// append the SVG container
-const svg = canvas.append('svg')
-  .attr('width', 600)
-  .attr('height', 600);
+d3.select('rect')
+  .data(data)
+  .attr('width', function(d,i,n){ return d.width })
+  .attr('height', function(d,i,n){ return d.height })
+  .attr('fill', function(d,i,n){ return d.fill });
 
-// create a group
-const group = svg.append('g')
-  .attr('transform', 'translate(0, 100)');
 
-// append the SVG elements to the SVG container
-group.append('rect')
-  .attr('width', 200)
-  .attr('height', 100)
-  .attr('fill', 'blue')
-  .attr('x', 20)
-  .attr('y', 20);
-
-group.append('circle')
-  .attr('r', 50)
-  .attr('cx', 300)
-  .attr('cy', 70)
-  .attr('fill', 'pink');
-
-group.append('line')
-  .attr('x1', 370)
-  .attr('x2', 400)
-  .attr('y1', 20)
-  .attr('y2', 120)
-  .attr('stroke', 'red');
-
-svg.append('text')
-  .attr('x', 20)
-  .attr('y', 200)
-  .attr('fill', 'grey')
-  .text('hello, ninjas')
-  .style('font-family', 'arial')
-  .attr('transform', 'translate(0, -100)');
