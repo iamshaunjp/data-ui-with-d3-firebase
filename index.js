429541ee53dd115b30b449ce8bbc0a1c3c06915b
Shaun <shaunjp@shaun-pelling.com>
2018-09-05 16:22:18 +0100

    lesson-12

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const canvas = d3.select('.canvas');
 
 // append the SVG container
-const svg = canvas.append('svg');
+const svg = canvas.append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
 
 // append the SVG elements to the SVG container
-svg.append('rect');
-svg.append('circle');
-svg.append('line');
+svg.append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20);
+
+svg.append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink');
+
+svg.append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red');
