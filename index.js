c144bbd01e1388b5fb16bf64ff940505a618a11d
Shaun <shaunjp@shaun-pelling.com>
2018-09-06 10:35:15 +0100

    lesson-16

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const svg = d3.select('svg');
 svg.select('rect')
   .data(data)
   .attr('width', function(d,i,n){ return d.width })
-  .attr('height', function(d,i,n){ return d.height })
-  .attr('fill', function(d,i,n){ return d.fill });
+  .attr('height', function(d){ return d.height })
+  .attr('fill', function(d){ return d.fill });
 
 
