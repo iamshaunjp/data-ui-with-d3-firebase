74328b157b5a9af2dd06d933499f931f46c05745
Shaun <shaunjp@shaun-pelling.com>
2018-09-06 19:00:31 +0100

    lesson-25

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-// select the svg conatiner first
-const svg = d3.select('svg');
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+// create margins & dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.json').then(data => {
 
@@ -13,14 +26,8 @@ d3.json('menu.json').then(data => {
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
-  // const min = d3.min(data, d => d.orders);
-  // const max = d3.max(data, d => d.orders);
-  // const extent = d3.extent(data, d => d.orders);
-
-  // console.log(min, max, extent);
-
   // join the data to circs
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data);
 
   // add attrs to circs already in the DOM
