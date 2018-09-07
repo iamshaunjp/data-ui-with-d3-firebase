d788d345140cb8daea822ec3f1adfeaf0efba047
Shaun <shaunjp@shaun-pelling.com>
2018-09-07 18:10:02 +0100

    lesson-39

--- index.js
+++ index.js
@@ -76,19 +76,32 @@ const update = (data) => {
 
 };
 
-db.collection('dishes').get().then(res => {
+var data = [];
 
-  var data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
+db.collection('dishes').onSnapshot(res => {
+  
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
   });
 
   update(data);
 
-  d3.interval(() => {
-    data.pop();
-    update(data)
-  }, 3000);
-  
 });
 
