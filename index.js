c96560e7552ef7f6a257f946e9247d863cca2fef
Shaun <shaunjp@shaun-pelling.com>
2018-09-10 14:51:38 +0100

    lesson-39

--- index.js
+++ index.js
@@ -82,26 +82,11 @@ db.collection('dishes').onSnapshot(res => {
   
   res.docChanges().forEach(change => {
 
-    const doc = {...change.doc.data(), id: change.doc.id};
-
-    switch (change.type) {
-      case 'added':
-        data.push(doc);
-        break;
-      case 'modified':
-        const index = data.findIndex(item => item.id == doc.id);
-        data[index] = doc;
-        break;
-      case 'removed':
-        data = data.filter(item => item.id !== doc.id);
-        break;
-      default:
-        break;
-    }
+    console.log(change.doc.data());
 
   });
 
-  update(data);
+  // update(data);
 
 });
 
