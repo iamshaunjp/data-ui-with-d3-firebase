e8f02d8a43fdf165c3b43e922e31b7e5372484ee
Shaun <shaunjp@shaun-pelling.com>
2018-09-11 17:22:20 +0100

    lesson-61

--- graph.js
+++ graph.js
@@ -20,14 +20,27 @@ const arcPath = d3.arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-// ordianl colour scale
+// ordinal colour scale
 const colour = d3.scaleOrdinal(d3["schemeSet3"]);
+
+// legend setup
+const legendGroup = svg.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`)
+
+const legend = d3.legendColor()
+  .shape('path', d3.symbol().type(d3.symbolCircle)())
+  .shapePadding(10)
+  .scale(colour)
 
 // update function
 const update = (data) => {
 
   // update colour scale domain
   colour.domain(data.map(d => d.name));
+
+  // update legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text').attr('fill', 'white');
   
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path')
