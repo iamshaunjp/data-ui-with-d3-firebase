0ef606be5d8c95cc20090524e22e2359f31f166a
Shaun <shaunjp@shaun-pelling.com>
2018-09-11 12:17:46 +0100

    lesson-53

--- graph.js
+++ graph.js
@@ -15,16 +15,46 @@ const pie = d3.pie()
   .value(d => d.cost);
   // the value we are evaluating to create the pie angles
 
-const angles = pie([
-  { name: 'rent', cost: 500 },
-  { name: 'bills', cost: 300 },
-  { name: 'gaming', cost: 200 }
-]);
-
 const arcPath = d3.arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-console.log(arcPath(angles[0]));
+// update function
+const update = (data) => {
+  
+  console.log(data);
+
+};
+
+// data array and firestore
+var data = [];
+
+db.collection('expenses').orderBy('cost').onSnapshot(res => {
+
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+  });
+
+  // call the update function
+  update(data);
+
+});
 
 
