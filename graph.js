64b27c1c41aa57024baa1997c1d33b253bd62e5f
Shaun <shaunjp@shaun-pelling.com>
2018-09-13 16:46:35 +0100

    lesson-72

---
+++ graph.js
@@ -0,0 +1,36 @@
+// update function
+const update = (data) => {
+
+  console.log(data);
+
+};
+
+// data and firestore
+var data = [];
+
+db.collection('activities').orderBy('date').onSnapshot(res => {
+
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+  });
+  
+  update(data);
+
+});
