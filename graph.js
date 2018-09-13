07ea2ae12af95a304990eda811d32f4b48c40e08
Shaun <shaunjp@shaun-pelling.com>
2018-09-13 17:03:01 +0100

    lesson-73

--- graph.js
+++ graph.js
@@ -1,3 +1,17 @@
+const margin = { top: 40, right: 20, bottom: 50, left: 100 };
+const graphWidth = 560 - margin.right - margin.left;
+const graphHeight = 400 - margin.top - margin.bottom;
+
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', graphWidth + margin.left + margin.right)
+  .attr('height', graphHeight + margin.top + margin.bottom);
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
 // update function
 const update = (data) => {
 
@@ -30,7 +44,7 @@ db.collection('activities').orderBy('date').onSnapshot(res => {
     }
 
   });
-  
+
   update(data);
 
 });
