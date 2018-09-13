9802c94e042a41d7d1e20b7c58138586d380547e
Shaun <shaunjp@shaun-pelling.com>
2018-09-13 16:30:15 +0100

    lesson-71

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var activity = 'cycling';
 
 btns.forEach(btn => {
   btn.addEventListener('click', e => {
-    // get activity
+    // get selected activity
     activity = e.target.dataset.activity;
 
     // remove and add active class
@@ -22,4 +22,25 @@ btns.forEach(btn => {
     // set text of form span (the activity)
     formAct.textContent = activity;
   });
+});
+
+// form submit
+form.addEventListener('submit', e => {
+  // prevent default action
+  e.preventDefault()
+
+  const distance = parseInt(input.value);
+  if(distance){
+    db.collection('activities').add({
+      distance, 
+      activity,
+      date: new Date().toString()
+    }).then(() => {
+      error.textContent = '';
+      input.value = '';
+    }).catch(err => console.log(err));
+  } else {
+    error.textContent = 'Please enter a valid distance'
+  }
+
 });
