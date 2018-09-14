2c1b0ad77e01a38663324534cb37a4f96ca1cf7b
Shaun <shaunjp@shaun-pelling.com>
2018-09-14 17:43:39 +0100

    lesson-88

--- index.js
+++ index.js
@@ -22,8 +22,29 @@ const data = [
   { name: 'classical', parent: 'music', amount: 5 },
 ];
 
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 1060)
+  .attr('height', 800);
+
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)'); // to give a 50px margin
+
 const stratify = d3.stratify()
   .id(d => d.name)
   .parentId(d => d.parent);
 
-console.log(stratify(data));
+const rootNode = stratify(data)
+  .sum(d => d.amount);
+
+const pack = d3.pack()
+  .size([960, 700])
+  .padding(5);
+
+// console.log(pack(rootNode));
+// console.log(pack(rootNode).descendants());
+
+const bubbleData = pack(rootNode).descendants();
+
+
+
