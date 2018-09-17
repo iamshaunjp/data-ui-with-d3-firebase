618d6a3ef16c32dcda595dd0ed27059a4ff3fa6f
Shaun <shaunjp@shaun-pelling.com>
2018-09-17 15:55:06 +0100

    lesson-92

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const modal = document.querySelector('.modal');
 M.Modal.init(modal);
+
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const parent = document.querySelector('#parent');
+const department = document.querySelector('#department');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  db.collection('employees').add({
+    name: name.value, 
+    parent: parent.value, 
+    department: department.value
+  });
+
+  var instance = M.Modal.getInstance(modal);
+  instance.close();
+
+  form.reset();
+
+});
